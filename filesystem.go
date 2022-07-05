41c2d19b9807c648e40790484ad00f44c512699d
eleven26 <rubymay21s@gmail.com>
2022-07-05 18:01:44 +0800

    base func

--- filesystem.go
+++ filesystem.go
@@ -1,6 +1,8 @@
 package fs
 
 import (
+	"io/fs"
+	"io/ioutil"
 	"os"
 )
 
@@ -12,4 +14,88 @@ func Exists(path string) (bool, error) {
 		// Therefore, do *NOT* use !os.IsNotExist(err) to test for file existence
 		return false, err
 	}
+}
+
+func Get(path string) (b []byte, err error) {
+	return os.ReadFile(path)
+}
+
+func GetString(path string) (content string, err error) {
+	b, err := Get(path)
+	if err != nil {
+		return "", err
+	}
+
+	return string(b), nil
+}
+
+func Put(path string, content []byte) error {
+	return ioutil.WriteFile(path, content, 0644)
+}
+
+func PutString(path string, content string) error {
+	return Put(path, []byte(content))
+}
+
+func Append(path string, content []byte) error {
+	f, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		return err
+	}
+	defer func(f *os.File) {
+		_ = f.Close()
+	}(f)
+
+	_, err = f.Write(content)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Prepend(path string, content []byte) error {
+	s, err := Get(path)
+	if err != nil {
+		return err
+	}
+
+	content = append(content, s...)
+	err = Put(path, content)
+	return err
+}
+
+func Chmod(path string, mode fs.FileMode) error {
+	return os.Chmod(path, mode)
+}
+
+func Delete(paths ...string) error {
+	for _, path := range paths {
+		err := os.Remove(path)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func Move(from string, to string) error {
+	return os.Rename(from, to)
+}
+
+func Copy(src, dst string) error {
+	// Read all content of src to data
+	data, err := ioutil.ReadFile(src)
+	if err != nil {
+		return err
+	}
+
+	// Write data to dst
+	err = ioutil.WriteFile(dst, data, 0644)
+	return err
+}
+
+func Link(oldname, newname string) error {
+	return os.Symlink(oldname, newname)
 }
--- filesystem_test.go
+++ filesystem_test.go
@@ -2,9 +2,11 @@ package fs
 
 import (
 	"errors"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"io/fs"
 	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -17,4 +19,159 @@ func TestExists(t *testing.T) {
 	assert.False(t, ok)
 	assert.True(t, errors.Is(err, os.ErrNotExist))
 	assert.True(t, errors.Is(err, fs.ErrNotExist))
+}
+
+func TestGet(t *testing.T) {
+	bs, err := Get("filesystem1.go")
+	assert.Empty(t, bs)
+	assert.True(t, errors.Is(err, os.ErrNotExist))
+	assert.True(t, errors.Is(err, fs.ErrNotExist))
+
+	bs, _ = Get("test/foo.txt")
+	assert.Equal(t, bs, []byte("abc"))
+}
+
+func TestGetString(t *testing.T) {
+	s, _ := GetString("test/foo.txt")
+	assert.Equal(t, s, "abc")
+}
+
+func TestPut(t *testing.T) {
+	path := "test/tmp/foo.txt"
+	err := Put(path, []byte("abc"))
+	defer func(name string) {
+		_ = os.Remove(name)
+	}(path)
+
+	assert.Equal(t, err, nil)
+
+	s, _ := GetString(path)
+	assert.Equal(t, s, "abc")
+}
+
+func TestPutString(t *testing.T) {
+	path := "test/tmp/foo.txt"
+	err := PutString(path, "abc")
+	defer func(name string) {
+		_ = os.Remove(name)
+	}(path)
+
+	assert.Equal(t, err, nil)
+
+	s, _ := GetString(path)
+	assert.Equal(t, s, "abc")
+}
+
+func TestAppend(t *testing.T) {
+	path := "test/tmp/foo.txt"
+	PutString(path, "abc")
+	defer func(name string) {
+		_ = os.Remove(name)
+	}(path)
+
+	Append(path, []byte("aaa"))
+	s, _ := GetString(path)
+	assert.Equal(t, s, "abcaaa")
+}
+
+func TestPrepend(t *testing.T) {
+	path := "test/tmp/foo.txt"
+	PutString(path, "abc")
+	defer func(name string) {
+		_ = os.Remove(name)
+	}(path)
+
+	Prepend(path, []byte("aaa"))
+	s, _ := GetString(path)
+	assert.Equal(t, s, "aaaabc")
+}
+
+func TestChmod(t *testing.T) {
+	path := "test/tmp/foo.txt"
+	PutString(path, "abc")
+	defer func(name string) {
+		_ = os.Remove(name)
+	}(path)
+
+	Chmod(path, 0600)
+	f, _ := os.Stat(path)
+	assert.Equal(t, "600", fmt.Sprintf("%o", f.Mode().Perm()))
+}
+
+func TestDelete(t *testing.T) {
+	path1 := "test/tmp/foo1.txt"
+	path2 := "test/tmp/foo2.txt"
+	PutString(path1, "abc")
+	PutString(path2, "def")
+
+	Delete(path1, path2)
+
+	exists, err := Exists(path1)
+	assert.False(t, exists)
+	assert.True(t, errors.Is(err, os.ErrNotExist))
+
+	exists, err = Exists(path2)
+	assert.False(t, exists)
+	assert.True(t, errors.Is(err, os.ErrNotExist))
+}
+
+func TestMove(t *testing.T) {
+	path1 := "test/tmp/foo1.txt"
+	path2 := "test/tmp/foo2.txt"
+	defer func(name string) {
+		_ = os.Remove(name)
+	}(path2)
+
+	PutString(path1, "abc")
+
+	Move(path1, path2)
+
+	exists, err := Exists(path1)
+	assert.False(t, exists)
+	assert.True(t, errors.Is(err, os.ErrNotExist))
+
+	exists, err = Exists(path2)
+	assert.True(t, exists)
+	assert.Nil(t, err)
+}
+
+func TestCopy(t *testing.T) {
+	path1 := "test/tmp/foo1.txt"
+	path2 := "test/tmp/foo2.txt"
+	defer func(name string) {
+		_ = os.Remove(name)
+	}(path1)
+	defer func(name string) {
+		_ = os.Remove(name)
+	}(path2)
+
+	PutString(path1, "abc")
+	Copy(path1, path2)
+
+	exists, err := Exists(path1)
+	assert.True(t, exists)
+	assert.Nil(t, err)
+
+	exists, err = Exists(path2)
+	assert.True(t, exists)
+	assert.Nil(t, err)
+}
+
+func TestLink(t *testing.T) {
+	path1 := "test/tmp/foo1.txt"
+	path2 := "test/tmp/foo2.txt"
+	defer func(name string) {
+		_ = os.Remove(name)
+	}(path1)
+	defer func(name string) {
+		_ = os.Remove(name)
+	}(path2)
+
+	PutString(path1, "abc")
+	p1, _ := filepath.Abs(path1)
+	p2, _ := filepath.Abs(path2)
+	Link(p1, p2)
+
+	s, _ := GetString(path2)
+	assert.Equal(t, s, "abc")
 }
